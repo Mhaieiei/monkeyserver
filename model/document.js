8bc3120f9b57e66e34b83bd6ad6854a15baaa788
iceonepiece <iceonepiece@gmail.com>
2016-04-18 02:58:13 +0700

    fixed diverged

--- model/document.js
+++ model/document.js
@@ -1,33 +1,85 @@
+/**
+ * A document schema.
+ * The document contains metadata about the file, owner, and its current status.
+ *
+ * This module defines a mongoose schema for 'Document'.
+ * Other schemas can inherit from this schema through a discrimination key.
+
+ * @author Apipol Niyomsak <makemek101@hotmail.com>
+ */
+
+// ----------------------------------------------------------------------------
+
+// Required libraries.
 var db = require('../lib/dbclient').db();
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+// key-valued options for configuring mongoose schema.
 var options = {discriminatorKey: 'department'}
 
 var docSchema = new Schema({
 
+	/*
+	 * The original user who create the document.
+	 */
 	owner: {
 		type: String,
 		ref: 'user'
 	},
 
+	/*
+	 * A date when this record is created.
+	 */
 	dateCreate: {
 		type: Date,
 		default: Date.now
 	},
+
+	/*
+	 * The filename including extension that is going to appear on the page.
+	 * It is the original filename that user uploaded.
+	 * If it is a by-product from the workflow, the system automatically generates it.
+	 */
 	name: String,
-	author: String,
+
+	/*
+	 * A boolean indicates whether this document is created by the system.
+	 * The default value for this field is false.
+	 */
+	is_auto_generate: {
+		type: Boolean,
+		default: false
+	},
+
+	/*
+	 * Indicates the current states document going through the workflow.
+	 * Initial status for every document is 'create'.
+	 * Status:
+	 *    - create: The document has just created. 
+	 *              It doesn't go throguh any workflow yet.
+	 *    - inprogress: The document is still processing inside the workflow.
+	 *    - done: The document has complete the execution inside the workflow. 
+	 *            Since during a workflow execution can produce multiple documents.
+	 *            Those documents will also have this status set as well.
+	 */
 	status: {
 		type: String,
 		enum: ['create', 'inprogress', 'done'],
 		default: 'create'
 	},
 	
+	/*
+	 * List of users who is assigned to perform some tasks in this document.
+	 */
 	_assignees: [{
 		type: Schema.Types.ObjectId,
 		ref: 'user'
 	}],
 
+	/*
+	 * Other required documents or other dependent documents.
+	 */
 	relate2docs: [{
 		type: Schema.Types.ObjectId,
 		ref: 'document'
@@ -38,27 +90,56 @@ var docSchema = new Schema({
 		ref: 'workflow'
 	}],
 
+	/*
+	 * Absolute path to the file stored in a server.
+	 * Note that the field 'name' doesn't have to be the same as 'filepath'
+	 */
 	filepath: String
+
 }, options);
 
+/**
+ * Find documents owned by user ID.
+ * @param {object} user object queried from mongoose.
+ * @return {object[]} list of document objects.
+ */
 docSchema.statics.findByUser = function(user) {
 	return this.find({'owner': user});
 };
 
+/**
+ * Set document's status for newly created document.
+ */
 docSchema.methods.created = function() {
 	this.status = this.schema.path('status').enumValues[0];
 };
+
+/**
+ * Set document's status as processing
+ */
 docSchema.methods.inProgress = function() {
 	this.status = this.schema.path('status').enumValues[1];
 };
+
+/**
+ * Set document's status as done.
+ */
 docSchema.methods.done = function() {
 	this.status = this.schema.path('status').enumValues[2];
 };
 
+/**
+ * Get the current status on this document.
+ * @return {string} status
+ */
 docSchema.methods.getStatus = function() {
 	return this.status;
 };
 
+/**
+ * Get a list of persons assigned in this document.
+ * @return {object[]} users - a user object[]
+ */
 docSchema.methods.assignees = function() {
 	return this._assignees;
 };
--- model/document/department/template.js
+++ model/document/department/template.js
@@ -1,6 +1,24 @@
+/**
+ * Subtype document schema templating module.
+ * @module model/document/department/template
+ * @author Apipol Niyomsak <makemek101@hotmail.com>
+ * 
+ * Create a schema inherited from {@link document} schema model with the following extra fields:
+ *  - docNum: An incremental integer field starting at 1.
+ *            It increments by 1 each time new record are saved automatically.
+ *            This field is a part of generating ID to identify records.
+ *            This field is controlled by 'mongoose-auto-increment'.
+ *            A plug-in in which increment an integer on a spcified fields.
+ * Please note that subtype name will be trimmed first before creating a schema.
+ * @throws error if the name already exists.
+ * @throws {InvalidArgumentException} if empty name is given.
+ * @param {string} abbriviation - A subtype document name.
+ * @param {object} [keyValue2Add] - optionally add key-valued pairs to the schema. Using the notation defined by mongoose.
+ * @return {object} inheritedSchema - A mongoose-modelled schema.
+ */
 module.exports = function(abbriviation, keyValue2Add) {
 	if(!abbriviation)
-		throw new Error('Invalid subtype name');
+		throw new InvalidArgumentException('Invalid subtype name');
 	abbriviation = abbriviation.trim();
 
 	var db = require('lib/dbclient').db();
