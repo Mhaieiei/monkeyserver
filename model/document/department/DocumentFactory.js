1ce374f818eaebf6a4ef7b7e8e031a9487072d18
Makemek <makemek101@hotmail.com>
2016-04-18 00:13:09 +0900

    [Code Documentation] Add documentation involved modules created by DMS

--- model/document/department/DocumentFactory.js
+++ model/document/department/DocumentFactory.js
@@ -1,3 +1,42 @@
+/**
+ * IC's subtype documents module.
+ * @module model/document/department/DocumentFactory
+ * @author Apipol Niyomsak <makemek101@hotmail.com>
+ * 
+ * This module contains a predefined IC's document templates types wrapped in JSON.
+ * They are all created by {@link template} which generates a mongoose schema with auto increment 'docnum' field.
+ * Documents in IC are divided into three categories.
+ * Abbrivations are used as subtype name to be recorded and queried.
+ * 
+ * 1. General Management 1
+ * ------------------------------------
+ * | Type              | Abbriviation |
+ * |-------------------|:------------:|
+ * | Archive           |      AA      |
+ * | Human Resources   |      AB      |
+ * | Quality Assurance |      AC      |
+ * | Parcel            |      AD      |
+ * | IT/KM             |      AE      |
+ * ------------------------------------
+ *
+ * 2. General Management 2
+ * -----------------------------------------
+ * | Type                   | Abbriviation |
+ * |------------------------|:------------:|
+ * | Plan & Risk Management |      BA      |
+ * | Accounting             |      BB      |
+ * | Research               |      BC      |
+ * | Academic Management    |      BD      |
+ * -----------------------------------------
+ *  
+ * 3. Academic
+ * ------------------------------------------
+ * | Type                    | Abbriviation |
+ * |-------------------------|:------------:|
+ * | Academic Administration |      CA      |
+ * | Student Affairs         |      CB      |
+ * ------------------------------------------
+ */
 var departmentDocTemplate = require('./template');
 
 module.exports = {
@@ -12,7 +51,7 @@ module.exports = {
 	riskManagement: departmentDocTemplate('BA'),
 	accounting: departmentDocTemplate('BB'),
 	research: departmentDocTemplate('BC'),
-	acadeicService: departmentDocTemplate('BD'),
+	acadeicManagement: departmentDocTemplate('BD'),
 
 	/* Academic */
 	academicAdministration: departmentDocTemplate('CA'),
