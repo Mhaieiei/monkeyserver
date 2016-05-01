5b6e4b1a68a972ac7c7300cbacdecaf15e971374
Mhaieiei <mai_junes@hotmail.com>
2016-05-01 21:55:09 +0700

    [AUN] aun10_1 handler

--- routes/direct.js
+++ routes/direct.js
@@ -824,25 +824,7 @@ app.use('/aun', aunController ); //aun handler
 
 
   
-  app.get('/aun10-1', isLoggedIn, function (req, res) {
-      console.log("FacilityAndInfrastrutureSchema");  
-      var acyear = req.query.acid;   
-      FacilityAndInfrastruture.find({ 'programAndAcYear': req.query.acid }, function (err, docs) {
-          if(err) console.log("aun10_1 query err"+err);
-          console.log("REFFFF---->>>", docs);
-          res.render('qa/qa-aun10.1.hbs', {
-            //    user: req.user,      
-            layout: "qaPage",
-            docs: docs,
-            acid : req.query.acid,
-            helpers: {
-                  inc: function (value) { return parseInt(value) + 1; },
-                  getacyear: function () { return acyear; }
-            } 
-        });
-    });            
-
-  });
+ 
 
  
   
@@ -1704,113 +1686,7 @@ app.use('/aun', aunController ); //aun handler
         });
 
   });
- //=====================================
-  // Edit QA ==============================
-  // =====================================
-
-  
-
-  //---------------------------edit aun 10.1 ----------------------------------------------------------------------
-  app.get('/addaun10_1',isLoggedIn,function(req,res){
-    console.log("[GET]Edit AUN 10.1");    
-    console.log(req.query.acid);
-   
-    res.render('qa/editqa/aun10.1_add_facilities.hbs', {
-            //    user: req.user,      
-            layout: "qaPage",
-            acid : req.query.acid
-        }); 
-    
-  });
-app.post('/addaun10_1',isLoggedIn,function(req,res){
-    console.log("[POST]Edit AUN 10.1");    
-    console.log(req.body.acid);
-    console.log(req.body.roomno);
-    console.log(req.body.floor);
-    console.log(req.body.building);
-    console.log(req.body.noofseat);
-   
-    FacilityAndInfrastruture.findOne({ 
-      $and: [
-                 { 'programAndAcYear' :  req.body.acid  },
-                 { 'roomNo' : req.body.roomno }
-               ]
-      
-    }, function (err, docs) {
-          if(err) console.log("aun10_1 query err"+err);
-          var facility = new FacilityAndInfrastruture();
-            facility.programAndAcYear = req.body.acid;
-            facility.roomNo = req.body.roomno;
-            facility.floor = req.body.floor;
-            facility.building = req.body.building;
-            facility.numberOfSeat = req.body.noofseat;
-           facility.save(function(err,manage) {
-            if (err){console.log('cant make new facility'+err);}  
-            else{
-              console.log(manage);
-              console.log("Insert new facility succesful");  
-              res.redirect('/aun10-1?acid='+req.body.acid);                          
-            }                         
-         });  
-         
-    });    
-    
-  });
-
- app.get('/editaun10_1',isLoggedIn,function(req,res){
-    var index =req.query.id;
-    console.log("[Get]Edit AUN 10.1");
-    return FacilityAndInfrastruture.findById(index, function( err, facility ) {
-        if( !err ) {
-        console.log(facility);
-            res.render('qa/editqa/aun10_1edit.hbs', {
-              layout: "adminPage",
-              facility: facility ,                     
-            });
-        } else {
-            return console.log( "query facility err"+err );
-          }
-      }); 
-  });
-  app.post('/editaun10_1',isLoggedIn,function(req,res){
-    console.log("[Post] Edit AUN10.1");
-    return FacilityAndInfrastruture.findById(req.body.facilityid, function( err, facility ) {
-        if( err ) {console.log('Query facility err'+err);}
-        console.log(facility);
-        facility.editFacility(req,res);          
-      }); 
-  });
-
-  app.get('/delaun10_1',isLoggedIn,function(req,res){
-    console.log("Delete Aun10.1");
-    console.log(req.query.id);
-    //console.log(req.query.email);
-
-    FacilityAndInfrastruture.remove(
-          { '_id' : req.query.id },
-          function(err, results) {
-            if (err){console.log('Delete facility err'+err);}
-          else console.log(results);
-          }
-       );
-    res.redirect('/aun10-1?acid='+req.query.acid);
-
-    
-    
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 //----------------------------------------------------------------------------------------------------------------------------
   //=====================================
   // Get Work Info.(Student) ==============================
