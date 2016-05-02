e0f2934bc67bd0f10ef2ba0a44301f7f607a137b
Mhaieiei <mai_junes@hotmail.com>
2016-05-02 21:21:27 +0700

    [AUN2-1] add feature

--- model/referenceCurriculumSchema.js
+++ model/referenceCurriculumSchema.js
@@ -5,23 +5,16 @@ var bcrypt = require('bcrypt-nodejs');
 // define the schema for our user model
 var referenceCurriculumSchema = mongoose.Schema({
 
-    programname:String,
-    detail: [{ type: mongoose.Schema.Types.ObjectId, ref: 'detail' }]
-
-});
-
-var detailSchema = mongoose.Schema({
-
-    refProgramName: String,
+   	refProgramName: String,
     degree: String,
     university: String,
     country: String,
     website: String,
-    
 
 });
 
+
+
 var referenceCurriculum = db.model('referenceCurriculum', referenceCurriculumSchema, 'referenceCurriculumSchema');
-referenceCurriculum.detail = db.model('detail', detailSchema, 'referenceCurriculumSchema');
 
 module.exports = referenceCurriculum;
