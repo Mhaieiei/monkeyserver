0b9095f111d1cbec27b436264c8166c4bd874e0f
Makemek <makemek101@hotmail.com>
2016-05-15 23:26:59 +0900

    test web page

---
+++ test/integration/routing/webpage/testDmsPages.js
@@ -0,0 +1,30 @@
+var rootpath = require('rootpath')();
+
+var db = require('test/dbTestConfig');
+
+var dbMock = require('test/dbTestConfig');
+var helper = require('test/helperFunction');
+var DmsServer = require('test/DmsServer');
+
+describe.only('DMS pages HTTP request testing', function() {
+
+	var server;
+
+	before(function(done) {
+		require('app')(db);
+		var app = require('app')(dbMock);
+		server = new DmsServer(app);
+		helper.registerAndLogin(server, 'joe', 'joe')(done);
+	})
+
+	describe('Homepage', function() {
+		pageShouldExist('/home');
+	})
+
+	function pageShouldExist(uri) {
+		return it('URI: ' + uri + ' exists', function(done) {
+			done();
+		})
+	}
+
+})
