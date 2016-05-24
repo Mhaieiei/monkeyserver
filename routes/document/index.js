974657e4e4eea28311428887d507fa2ebf3f24e5
Makemek <makemek101@hotmail.com>
2016-05-24 23:32:50 +0900

    query role type groupped by position

--- routes/document/index.js
+++ routes/document/index.js
@@ -1,10 +1,32 @@
 'use strict';
 
 var router = require('express').Router();
+var Document = require('model/document/document');
+var Role = require('model/role');
 var isLoggedIn = require('middleware/loginChecker');
+var async = require('async');
 
 router.get('/:docId/visibility', isLoggedIn, function(req, res, next) {
-	res.render('document/visibility.hbs', {layout: 'homePage'})
+	var response = {layout: 'homePage'};
+	response.docId = req.params.docId;
+
+	var showRoleField = {objectId: "$_id", type: "$type"};
+	var aggregatePosition = {$push: "$position"};
+	var roleTypeGroup = {
+		$group: {
+			_id: showRoleField,
+			position: aggregatePosition
+		}
+	}
+	
+	Role.aggregate([roleTypeGroup])
+	.exec(function(error, roleGroup) {
+		if(error) next(error);
+
+		console.log(roleGroup);
+		res.render('document/visibility.hbs', response);
+	})
+
 })
 
 module.exports = exports = router;
